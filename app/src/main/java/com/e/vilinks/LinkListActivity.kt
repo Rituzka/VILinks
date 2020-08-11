8fe20b8842a811dfcb9bcf1fd83d0c638ae7b022
Rita Casiello <rituzka@gmail.com>
2020-08-11 16:21:52 +0200

    menu added and function onsetlistener to the floating button

--- app/src/main/java/com/e/vilinks/LinkListActivity.kt
+++ app/src/main/java/com/e/vilinks/LinkListActivity.kt
@@ -2,10 +2,32 @@ package com.e.vilinks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_list_links.*
 
 class LinkListActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_links)
+
+        addLink.setOnClickListener { view ->
+            Snackbar.make(view, "action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
